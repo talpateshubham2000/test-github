e7cd2d172787447683706692d04993c12d60a74e
talpateshubham2000 <talpateshubham2000@gmail.com>
2026-07-05 12:16:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,24 +9,3 @@ const b = 30;
 console.log(b);
 console.log(a + b);
 
-function addNumbers(x, y) {
-    return x + y;
-}
-
-
-function subtractNumbers(x, y) {
-    return x - y;
-}
-
-
-function multiplyNumbers(x, y) {
-    return x * y;
-}
-
-
-function divideNumbers(x, y) {
-    if (y === 0) {
-        throw new Error("Cannot divide by zero");
-    }
-    return x / y;
-}
